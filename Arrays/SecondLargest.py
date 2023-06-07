55387f20842417bf73d11dadbcf280fa5f4270ea
Purushotham Kumar <pkalive@collegeboard.org>
2023-06-07 20:29:46 +0530

    second_largest

---
+++ Arrays/SecondLargest.py
@@ -0,0 +1,18 @@
+"""
+    You are given an integer array A. You have to find the second-largest element/value in the array or report that no such element exists.
+"""
+
+
+def second_largest(arr):
+    largest, next_largest = -1, -1
+    for num in arr:
+        if num > largest:
+            largest = num
+        if largest > num > next_largest:
+            next_largest = num
+
+    return next_largest
+
+
+print(second_largest([2, 1, 2]))
+print(second_largest([2]))
