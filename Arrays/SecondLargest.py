7ea30d711748059deab11bd2fed76c44c71e325f
Purushotham Kumar <pkalive@collegeboard.org>
2023-06-07 20:38:48 +0530

    second_largest

--- Arrays/SecondLargest.py
+++ Arrays/SecondLargest.py
@@ -7,6 +7,7 @@ def second_largest(arr):
     largest, next_largest = -1, -1
     for num in arr:
         if num > largest:
+            next_largest = largest
             largest = num
         if largest > num > next_largest:
             next_largest = num
@@ -15,4 +16,5 @@ def second_largest(arr):
 
 
 print(second_largest([2, 1, 2]))
-print(second_largest([2]))
+print(second_largest([11,15,19]))
+print(second_largest([13, 7, 16, 18, 14, 17, 18, 8, 10]))
